4cb894ae3ce85ae64e3ff7049a8d2d02331946dc
Kenny <kennyquintana297@gmail.com>
2021-04-05 00:13:15 -0400

    pregunta4

--- preg1.ts
+++ preg1.ts
@@ -1,14 +1,38 @@
-var palabra: string = "anitalavalatina";
-console.log(palindromo(palabra));
+var vector :number[] = [3, 6, -2, 10, 7, 3];
+var maximo:number = 0;
 
-function palindromo(cad:string):boolean {
-    let newstring:string = "";
-    for(var i = cad.length - 1; i>=0; i--) {
-        newstring += cad.charAt(i);
+console.log(producto(0));
+/*function comp() {} ///funcion vacia para que no afecte a mi process.on
+
+process.stdin.on('data',function(data) {
+    vector.push(parseInt(data.toString().trim()));
+
+    if(vector.length < tamaño){
+        comp();
     }
-    if (palabra === newstring) {
-        return true;
-    } 
-    //console.log(newstring);
-    return false;
+    else{
+        /*for(var i=0;i<tamaño;i++) {
+            process.stdout.write(vector[i]+" ");
+        }
+
+        //console.log(vector.length);
+        console.log(producto(0));
+
+      
+
+        process.exit();
+    }
+   
+});*/
+
+
+
+function producto(pos):number {
+    if(pos>=vector.length-1)
+        return maximo;
+    else{
+        if((vector[pos]*vector[pos+1])>maximo)
+            maximo=vector[pos]*vector[pos+1];
+        return producto(pos+1); 
+    }
 }
